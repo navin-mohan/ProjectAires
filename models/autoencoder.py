0761bfed7c8ac8332de7521b1600ff4992671323
Navin Mohan <navinmohan81@gmail.com>
2017-04-29 17:16:54 +0530

    added generate function to datagen.py

---
+++ models/autoencoder.py
@@ -0,0 +1,8 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+
+model = Sequential()
+
+
--- models/datagen.py
+++ models/datagen.py
@@ -1,29 +1,25 @@
 import numpy as np
-
-SIZE = 30
-
-np.random.seed(10)
-
-visibility = np.abs(np.random.normal(loc=2.0,scale=1.5,size=SIZE))
-age		   = np.abs(np.random.normal(loc=44,scale=20,size=SIZE)).astype(np.integer)
-weather	   = np.append(np.random.choice(np.array([0,1]),int(SIZE*0.99)), np.random.choice(np.array([2,3]),int(SIZE*0.01)))
-
-if weather.shape[0] != SIZE:
-	weather = np.append(weather,np.random.choice(np.array([0,1,2,3]),SIZE-weather.shape[0])) 
-
-road_quality = np.abs(np.random.normal(loc=0.3,scale=1,size=SIZE))
-accelerometer = np.abs(np.random.normal(loc=2.2,scale=1,size=SIZE))
-speed		= 2*np.multiply(road_quality,np.random.normal(loc=60,scale=0.1,size=SIZE)) + np.random.uniform(low=0,high=10,size=SIZE)
+import pandas as pd 
 
 
-dataset = np.array([
-		visibility,
-		age,
-		weather,
-		road_quality,
-		accelerometer,
-		speed
-	]).T.astype(np.float32)
+def generate_data(batch_size,seed=10):
 
-print(dataset)
+	np.random.seed(seed)
+	SIZE = batch_size
+	visibility = np.abs(np.random.normal(loc=2.0,scale=1.5,size=SIZE))
+	age		   = np.abs(np.random.normal(loc=44,scale=20,size=SIZE)).astype(np.integer)
+	weather	   = np.append(np.random.choice(np.array([0,1]),int(np.floor(SIZE*0.99))), np.random.choice(np.array([2,3]),int(np.ceil(SIZE*0.01)))) 
+	road_quality = np.abs(np.random.normal(loc=0.3,scale=1,size=SIZE))
+	accelerometer = np.abs(np.random.normal(loc=2.2,scale=1,size=SIZE))
+	speed		= 2*np.multiply(road_quality,np.random.normal(loc=60,scale=0.1,size=SIZE)) + np.random.uniform(low=0,high=10,size=SIZE)
+
+
+	return np.array([
+			visibility,
+			age,
+			weather,
+			road_quality,
+			accelerometer,
+			speed
+		]).T.astype(np.float32)
 
